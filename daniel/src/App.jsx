16405ae0b15f8d75088746b05b11ec4780fcb42f
ThibDaniel <160511547+ThibDaniel@users.noreply.github.com>
2024-06-04 09:26:02 +0200

    Update App.jsx

--- daniel/src/App.jsx
+++ daniel/src/App.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
 import './App.css'
 import { HelloWorld } from './HelloWorld'
+import{Message} from './Message'
 import { Welcome } from './Welcome'
+import { Clock } from './Clock'
+
 
 
 function App() {
@@ -11,7 +14,11 @@ function App() {
     <>     
       <HelloWorld/>
       <HelloWorld/>
+      <Message></Message>
       <Welcome name={<strong>World</strong>} age= "18"/>
+      <Clock></Clock>
+     
+      
     </>
   )
 }
