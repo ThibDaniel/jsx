fa4a48ecc376006bdd5039266cabd7a7f4352670
ThibDaniel <160511547+ThibDaniel@users.noreply.github.com>
2024-06-04 09:45:21 +0200

    useState

--- daniel/src/App.jsx
+++ daniel/src/App.jsx
@@ -4,8 +4,7 @@ import { HelloWorld } from './HelloWorld'
 import{Message} from './Message'
 import { Welcome } from './Welcome'
 import { Clock } from './Clock'
-
-
+import { Counter } from './CounterInc'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,6 +16,7 @@ function App() {
       <Message></Message>
       <Welcome name={<strong>World</strong>} age= "18"/>
       <Clock></Clock>
+      <Counter></Counter>
      
       
     </>
---
+++ daniel/src/CounterInc.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export function Counter() {
+    const [counter, SetCounter] = useState(0)
+    function Increment() {
+        SetCounter((c) => c + 1)
+    }
+    function Decrement(){
+        SetCounter((c) => c - 1)
+    }
+    function Reset() {
+        SetCounter((c) => 0)
+    }
+    return(
+        <div>
+            <h2>Counter: {counter}</h2>
+            <button onClick={Increment}>Increment</button>
+            <button onClick={Decrement}>Decrement</button>
+            <button onClick={Reset}>Reset</button>
+        </div>
+    )
+}
