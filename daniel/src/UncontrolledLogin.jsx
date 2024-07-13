a3d9916b960e6101f275fb6ad3e5126eb24a67b6
ThibDaniel <160511547+ThibDaniel@users.noreply.github.com>
2024-07-13 11:40:35 +0200

    Update UncontrolledLogin.jsx

--- daniel/src/UncontrolledLogin.jsx
+++ daniel/src/UncontrolledLogin.jsx
@@ -1,9 +1,37 @@
-export const UncontrolledLogin = () => {
-    return(
-        <form>
-            <input name="username" type="text" />
-            <input name="password" type="password" />
-            <input name="remember" type="checkbox" />
+class UncontrolledLogin extends React.Component {
+    handleSubmit = (event) => {
+      event.preventDefault();
+      
+      const formData = new FormData(event.target);
+  
+      const username = formData.get('username');
+      const password = formData.get('password');
+      
+      console.log('Username:', username);
+      console.log('Password:', password);
+  
+      if (username === 'user' && password === 'pass') {
+        alert('Login successful!');
+      } else {
+        alert('Login failed!');
+      }
+    };
+  
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor="username">Username:</label>
+            <input type="text" id="username" name="username" required />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" required />
+          </div>
+          <button type="submit">Login</button>
         </form>
-    )
-}
+      );
+    }
+  }
+  
+  export default UncontrolledLogin;
