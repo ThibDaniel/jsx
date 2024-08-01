05f6f0adb233b9590bffe72a06e53224712ae93a
ThibDaniel <160511547+ThibDaniel@users.noreply.github.com>
2024-08-01 12:17:39 +0200

    Create GithubUser5.jsx

---
+++ daniel/src/GithubUser5.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import GithubUsers from './GithubUsers';
+import ShowGithubUser from './ShowGithubUser';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>GitHub Users</h1>
+        <Router>
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/users">Users</Link></li>
+              <li><Link to="/users/octocat">User: Octocat</Link></li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<GithubUsers />}>
+              <Route index element={<div>Add a user and select it</div>} />
+            </Route>
+            <Route path="/users" element={<GithubUsers />}>
+              <Route index element={<div>Add a user and select it</div>} />
+            </Route>
+            <Route path="/users/:username" element={<ShowGithubUser />} />
+          </Routes>
+        </Router>
+      </header>
+    </div>
+  );
+}
+
+export default App;
